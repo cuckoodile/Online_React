c31ad1d91ed4a8cb9540229f66e6293513b2efc1
ryandelossantos <ryanrinon0913@gmail.com>
2025-05-02 12:58:45 +0800

    with api functions and hooks

---
+++ src/utils/hooks/useProductsHooks.js
@@ -0,0 +1,41 @@
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
+import { createProduct,updateProduct,fetchProducts,deleteProduct} from "../productApi";
+
+export const useProducts = () => {
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: fetchProducts,
+    staleTime: 1000 * 60 * 5,
+  });
+};
+
+export const useCreateProduct = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: createProduct,
+      onSuccess: () => {
+        queryClient.invalidateQueries(["products"]);
+      },
+    });
+  };
+
+  export const useUpdateProduct = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: ({ id, updatedData }) => updateProduct(id, updatedData),
+      onSuccess: () => {
+        queryClient.invalidateQueries(["products"]);
+      },
+    });
+  };
+
+  export const useDeleteProduct = () => {
+    const queryClient = useQueryClient();
+  
+    return useMutation({
+      mutationFn: deleteProduct,
+      onSuccess: () => {
+        queryClient.invalidateQueries(["products"]);
+      },
+    });
+  };
