c17a6d587e95d363f803500b29d25644eac3bec9
ryandelossantos <ryanrinon0913@gmail.com>
2025-05-21 12:34:53 +0800

    Fixed some Bugs Encountered On Add To Cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,60 +1,45 @@
 import React, { useContext } from "react";
 import { motion } from "framer-motion";
 import { FiShoppingCart, FiHeart, FiSearch } from "react-icons/fi";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAddCartItem } from "../utils/hooks/useCartsHooks";
-import { useUsers } from "../utils/hooks/userUsersHooks";
 import { AuthContext } from "@/utils/contexts/AuthContext";
 
 export default function Card({ data: product }) {
   const navigate = useNavigate();
   const addCartItem = useAddCartItem();
-  
-  
+  const { user } = useContext(AuthContext); 
+
   const handleNavigate = (path) => {
     navigate(`${path}?id=${product.id}`);
   };
 
   const handleAddToCart = (e) => {
-    const { user } = useContext(AuthContext);
-    if(user){
-    const token = user?.token;
-    const {
-      data: userData,
-      error: userError,
-      isLoading
-    } = useUsers(user);
-  
-
     e.stopPropagation();
-    if (isLoading) {
-      console.log("User data is still loading...");
-      return;
-    }
-    console.log("User data:", userData.data[0].id);
-    console.log("User error:", userError);
-    if (userData.data && userData.data[0].id) {
-      const user_id = userData.data[0].id; 
-      const payload = { product_id: product.id, quantity: 1, user_id };
-      console.log("Payload being sent to backend:", payload);
-      console.log("Token being sent to backend:", token);
-      addCartItem.mutate({
-        data:payload,
-        token: token,
-      },{
-        onError: (error) => {
-          console.error("Error adding item to cart:", error.response?.data || error.message);
-        },
-      });
-    } else {
-      console.error("User data is undefined or missing user_id.");
-    }
-  }else{
-    Navigate('/login')
-  }
-  };
+    if (user) {
+      const token = user?.token;
+      console.log("User object:", user);
+      const user_id = user.id || (user.data && Array.isArray(user.data) && user.data[0]?.id);
 
-  // console.log("Product Image Parsed:", product.image);
+      if (user_id) {
+        const payload = { product_id: product.id, quantity: 1, user_id };
+        console.log("Payload being sent to backend:", payload);
+        console.log("Token being sent to backend:", token);
+        addCartItem.mutate({
+          data: payload,
+          token: token,
+        }, {
+          onError: (error) => {
+            console.error("Error adding item to cart:", error.response?.data || error.message);
+          },
+        });
+      } else {
+        console.error("User data is undefined or missing user_id.");
+      }
+    } else {
+      navigate('/login');
+    }
+  };
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
